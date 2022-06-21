440cbd7bb3b7ad3db3e84ebeec722d5808246475
dulee <dulee.dev@gmail.com>
2022-06-21 23:10:34 +0900

    add env file path

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log(`\t🚀 server run on port:\t${process.env.PORT}`);
   console.log(`\t🐱 mode:\t\t${process.env.NODE_ENV}`);
+  console.log(`\t👻 env file path:\t.env.${process.env.NODE_ENV}`);
   console.log(`\t🎮 enter with:\t\thttp://localhost:${process.env.PORT}`);
   console.log();
   const app = await NestFactory.create(AppModule, { cors: true });
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT);
 }
 bootstrap();
-
-console.log(process.env.SAMPLE_ENV);
